6c72dc69f559b48c48821bbbbee13c590b180195
zahraa Meky <eng.alzahraa.meky@gmail.com>
2024-08-20 20:46:50 +0400

    add portofolio

--- data/index.ts
+++ data/index.ts
@@ -106,14 +106,14 @@ export const projects = [
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/hygraph.svg", "/c.svg"],
     link: "/ui.aiimg.com",
   },
-  // {
-  //   id: 4,
-  //   title: "Animated Apple Iphone 3D Website",
-  //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-  //   img: "/p4.svg",
-  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-  //   link: "/ui.apple.com",
-  // },
+  {
+    id: 4,
+    title: "AI Travel Planner App",
+    des: "Full-stack React Native app offering personalized travel recommendations and trip management.",
+    img: "/AiTravel.svg",
+    iconLists: ["/r.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
 ];
 
 export const testimonials = [
